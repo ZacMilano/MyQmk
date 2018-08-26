4641ccff1353d4fe8189577629f5e91ad422c230
Zac Monroe <zmonroe0820@gmail.com>
2018-08-26 16:51:00 -0400

    Fixed formatting

--- keyboards/planck/keymaps/zacs_made_online_keymap/keymap.c
+++ keyboards/planck/keymaps/zacs_made_online_keymap/keymap.c
@@ -50,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    */
 	[0] = KEYMAP(
-      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,           KC_Y,           KC_U,                 KC_I,         KC_O,           KC_P,         KC_BSPC,
-      KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,           KC_H,           KC_J,                 KC_K,         KC_L,           KC_SCLN,      KC_QUOT,
-      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,           KC_N,           KC_M,                 KC_COMM,      KC_DOT,         KC_SLSH,      RSFT_T(KC_ENT),
-      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_LCTL,        KC_SPC,         MO(5),                KC_NO,        KC_MUTE,        KC_VOLD,      KC_VOLU
+      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,           KC_Y,         KC_U,                 KC_I,         KC_O,           KC_P,         KC_BSPC,
+      KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,           KC_H,         KC_J,                 KC_K,         KC_L,           KC_SCLN,      KC_QUOT,
+      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,           KC_N,         KC_M,                 KC_COMM,      KC_DOT,         KC_SLSH,      RSFT_T(KC_ENT),
+      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_LCTL,        KC_SPC,       MO(5),                KC_NO,        KC_MUTE,        KC_VOLD,      KC_VOLU
       ),
 
 // Gaming layer
@@ -70,10 +70,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    */
 	[1] = KEYMAP(
-      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,           KC_Y,           KC_U,               KC_I,         KC_O,           KC_P,         KC_BSPC,
-      KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,           KC_H,           KC_J,               KC_K,         KC_L,           KC_SCLN,      KC_QUOT,
-      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,           KC_N,           KC_M,               KC_COMM,      KC_DOT,         KC_SLSH,      RSFT_T(KC_ENT),
-      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_SPC,         KC_SPC,         MO(5),              KC_LCTL,      KC_MUTE,        KC_VOLD,      KC_VOLU
+      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,           KC_Y,         KC_U,               KC_I,         KC_O,           KC_P,         KC_BSPC,
+      KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,           KC_H,         KC_J,               KC_K,         KC_L,           KC_SCLN,      KC_QUOT,
+      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,           KC_N,         KC_M,               KC_COMM,      KC_DOT,         KC_SLSH,      RSFT_T(KC_ENT),
+      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_SPC,         KC_SPC,       MO(5),              KC_LCTL,      KC_MUTE,        KC_VOLD,      KC_VOLU
       ),
 
 // Dvorak layer
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    */
 	[2] = KEYMAP(
-      KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,           KC_F,           KC_G,               KC_C,         KC_R,           KC_L,         KC_BSPC,
-      KC_ESC,   KC_A,     KC_O,     KC_E,     KC_U,     KC_I,           KC_D,           KC_H,               KC_T,         KC_N,           KC_S,         KC_SLSH,
-      KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,           KC_B,           KC_M,               KC_W,         KC_V,           KC_Z,         RSFT_T(KC_ENT),
-      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_SPC,         KC_SPC,         MO(5),              KC_LCTL,      KC_MUTE,        KC_VOLD,      KC_VOLU
+      KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,           KC_F,         KC_G,               KC_C,         KC_R,           KC_L,         KC_BSPC,
+      KC_ESC,   KC_A,     KC_O,     KC_E,     KC_U,     KC_I,           KC_D,         KC_H,               KC_T,         KC_N,           KC_S,         KC_SLSH,
+      KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,           KC_B,         KC_M,               KC_W,         KC_V,           KC_Z,         RSFT_T(KC_ENT),
+      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_SPC,         KC_SPC,       MO(5),              KC_LCTL,      KC_MUTE,        KC_VOLD,      KC_VOLU
       ),
 
 // F-key, numpad layer
@@ -110,10 +110,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    */
 	[3] = KEYMAP(
-      KC_TAB,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_NO,          KC_PSLS,        KC_P7,              KC_P8,        KC_P9,          KC_PMNS,      KC_BSPC,
-      KC_ESC,   KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_NO,          KC_PAST,        KC_P4,              KC_P5,        KC_P6,          KC_PPLS,      KC_DEL,
-      KC_LSFT,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,        KC_NO,          KC_P1,              KC_P2,        KC_P3,          KC_NO,        KC_ENT,
-      KC_TRNS,  MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_LCTL,        KC_SPC,         MO(5),              KC_P0,        KC_PDOT,        KC_PCMM,      KC_NLCK
+      KC_TAB,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_NO,          KC_PSLS,      KC_P7,              KC_P8,        KC_P9,          KC_PMNS,      KC_BSPC,
+      KC_ESC,   KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_NO,          KC_PAST,      KC_P4,              KC_P5,        KC_P6,          KC_PPLS,      KC_DEL,
+      KC_LSFT,  KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,        KC_NO,        KC_P1,              KC_P2,        KC_P3,          KC_NO,        KC_ENT,
+      KC_TRNS,  MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_LCTL,        KC_SPC,       MO(5),              KC_P0,        KC_PDOT,        KC_PCMM,      KC_NLCK
       ),
 
 // Numbers, common coding symbols
@@ -130,10 +130,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    */
 	[4] = KEYMAP(
-      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,           KC_6,           KC_7,                 KC_8,         KC_9,           KC_0,         KC_BSPC,
-      KC_ESC,   KC_LCBR,  KC_RCBR,  KC_HOME,  KC_END,   KC_NO,          KC_NO,          KC_MINS,              KC_EQL,       LSFT(KC_MINS),  LSFT(KC_EQL), KC_DEL,
-      KC_LSFT,  KC_LBRC,  KC_RBRC,  KC_LPRN,  KC_RPRN,  KC_NO,          KC_NO,          LSFT(KC_BSLS),        KC_COMM,      KC_DOT,         KC_BSLS,      KC_ENT,
-      TO(3),    MO(6),    KC_LGUI,  KC_LALT,  KC_TRNS,  KC_LCTL,        KC_SPC,         MO(5),                KC_CAPS,      KC_NO,          KC_NO,        KC_NO
+      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,           KC_6,         KC_7,               KC_8,         KC_9,           KC_0,         KC_BSPC,
+      KC_ESC,   KC_LCBR,  KC_RCBR,  KC_HOME,  KC_END,   KC_NO,          KC_NO,        KC_MINS,            KC_EQL,       LSFT(KC_MINS),  LSFT(KC_EQL), KC_DEL,
+      KC_LSFT,  KC_LBRC,  KC_RBRC,  KC_LPRN,  KC_RPRN,  KC_NO,          KC_NO,        KC_PIPE,            KC_COMM,      KC_DOT,         KC_BSLS,      KC_ENT,
+      TO(3),    MO(6),    KC_LGUI,  KC_LALT,  KC_TRNS,  KC_LCTL,        KC_SPC,       MO(5),              KC_CAPS,      KC_NO,          KC_NO,        KC_NO
       ),
 
 // Num-row symbols, arrow keys, browsing shortcuts
@@ -161,9 +161,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
 |         |Default:Q|Default:D|         |         |         |         |         |         |Music Tog|         |Beeps Tog|
 +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-|         |Default:G|Default:#|         |         |         |         |         |         |Mus Types|         |More beep|
+|         |Default:G|Default:#|         |         |         |         |         |         |Mus Types|         | Pitch + |
 +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
-|         |         |         |         |         |         |         |         |         |         |         |Less beep|
+|         |         |         |         |         |         |         |         |         |         |         | Pitch - |
 +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
 |         |   vvv   |         |         |         |         |Boot Mode|         |         |         |         |         |
 +---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+---------+
