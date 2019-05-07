0c7e55cccd0b5f154cfcb3a75ac923fcdb6206b8
Zac Monroe <zmonroe0820@gmail.com>
2019-05-07 12:12:00 -0400

    Added sleep function on bottom

--- keyboards/planck/keymaps/zacs_made_online_keymap/keymap.c
+++ keyboards/planck/keymaps/zacs_made_online_keymap/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,           KC_Y,         KC_U,                 KC_I,         KC_O,           KC_P,         KC_BSPC,
       KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,           KC_H,         KC_J,                 KC_K,         KC_L,           KC_SCLN,      KC_QUOT,
       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,           KC_N,         KC_M,                 KC_COMM,      KC_DOT,         KC_SLSH,      RSFT_T(KC_ENT),
-      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_LCTL,        KC_SPC,       MO(5),                KC_RALT,      _______,        _______,      LALT(KC_D)
+      MO(3),    MO(6),    KC_LGUI,  KC_LALT,  MO(4),    KC_LCTL,        KC_SPC,       MO(5),                KC_RALT,      _______,   LSFT(LALT(KC_F10)),LALT(KC_D)
       ),
 
 // Gaming layer
